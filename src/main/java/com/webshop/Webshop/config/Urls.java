79b749b25454014453bcb626883872db2fe58e1d
Konn25 <koncz.bence9@gmail.com>
2024-08-14 21:00:08 +0200

    add urls

--- src/main/java/com/webshop/Webshop/config/Urls.java
+++ src/main/java/com/webshop/Webshop/config/Urls.java
@@ -5,10 +5,15 @@ import lombok.Getter;
 @Getter
 public enum Urls {
 
-    ADMIN("/v1/users/**"),
-    USER("/v1/orders/**"),
+    ADMIN("/v1/users/**", "/v1/picture/**", "/v1/products/**", "/v1/product/type/**"),
+    USER("/v1/orders/**", "/v1/order/item/**", "/v1/order/status/**", "/v1/picture/all", "/v1/picture/{id}",
+         "/v1/products/**", "/v1/product/type/**" ,"/v1/users/**"
+    ),
 
-    NOT_LOGGED_IN("/v1/auth/authenticate","/v1/auth/register");
+    NOT_LOGGED_IN("/v1/auth/authenticate","/v1/auth/register", "/swagger-ui/**", "/v3/api-docs/**","/v2/api-docs/**",
+                  "/swagger-resources/**", "/v1/currency/**"
+
+                  );
 
     private final String[] urls;
 
