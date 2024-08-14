d1a544504752f32d64a584e60a50a1fc34d13e27
Konn25 <koncz.bence9@gmail.com>
2024-08-14 21:11:18 +0200

    add comment

--- src/main/java/com/webshop/Webshop/service/CurrencyService.java
+++ src/main/java/com/webshop/Webshop/service/CurrencyService.java
@@ -14,11 +14,20 @@ public class CurrencyService implements CurrencyServiceInterface {
 
     private final CurrencyRepository currencyRepository;
 
+    /**
+     *  Get a currency by id form the database
+     * @param id
+     * @return the right currency
+     */
     @Override
     public Optional<Currency> getCurrencyById(Long id) {
         return currencyRepository.findCurrencyById(id);
     }
 
+    /**
+     * Delete a currency by id from the database
+     * @param id
+     */
     @Override
     public void deleteCurrency(Long id) {
         Optional<Currency> foundCurrency = currencyRepository.findCurrencyById(id);
@@ -29,10 +38,21 @@ public class CurrencyService implements CurrencyServiceInterface {
 
     }
 
+    /**
+     * Create a new currency to the database
+     * @param currency
+     * @return Save a currency to the database
+     */
     @Override
     public Currency createNewCurrency(Currency currency) {
        return currencyRepository.save(currency);
     }
+
+    /**
+     * Update a currency
+     * @param currency
+     * @return Save an updated currency
+     */
 
     @Override
     public Currency updateNewCurrency(Currency currency) {
@@ -44,6 +64,10 @@ public class CurrencyService implements CurrencyServiceInterface {
         return currencyRepository.save(currency);
     }
 
+    /**
+     * Get all currency from the database
+     * @return All currency
+     */
     @Override
     public List<Currency> getAllCurrency() {
         return currencyRepository.findAll();
